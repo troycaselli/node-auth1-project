925b158f8229ba25bbe3625440257fe9d6201468
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-12-06 15:20:07 -0800

    created findBy model and updated find model

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -65,6 +65,5 @@ router.get("/", (req, res) => {
   }
  */
 
- 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,22 +3,15 @@ const db = require('../../data/db-config');
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
-async function find(req, res, next) {
-  try {
-    const users = await db('users');
-    return users.map(user => {
-      return {user_id: user.user_id, username: user.username};
-    })
-  } catch(err) {
-    next(err);
-  }
+async function find() {
+    return db('users').select('user_id', 'username');
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db('users').where(filter).select('user_id', 'username');
 }
 
 /**
